e02d639b88fb12fb3b8c31f601f78ed51e27e16c
whitmandeb <debbie.whitman01@yahoo.com>
2025-04-21 21:49:43 -0400

    week 5 lab 2

---
+++ script.js
@@ -0,0 +1,42 @@
+//  Part 1: For Loops
+
+// Problem 1: Count to 10
+
+for (let i = 0; i < 10; i++) {
+    console.log(i+1);
+  }
+
+
+
+  // Problem 2: Even Numbers
+
+for (let i = 0; i < 21; i+=2) {
+    console.log(i)
+}
+
+
+
+// Problem 3: Loop through an Array
+
+let animals = ["dog", "cat", "rabbit", "parrot"];
+for(let i = 0; i < animals.length; i++){
+    console.log("I like", animals[i]);
+}
+
+
+
+// Problem 4: Reverse Array Output
+/*
+let colors = ["red", "green", "blue", "yellow"];
+for(let i = colors.length; i > 0; i--){
+    colors.shift();
+    colors.unshift(colors[i]);
+}
+console.log(colors);
+*/
+
+
+// Part 2: Objects
+
+// Problem 5: Simple Object
+
